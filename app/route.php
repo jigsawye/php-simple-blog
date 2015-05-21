bc6c026c149d54a839358a6afce2c6e078288b50
jigsawye <jigsaw.ye@gmail.com>
2015-05-21 12:53:56 +0800

    Add file and update route of Authentication

--- app/route.php
+++ app/route.php
@@ -1,22 +1,30 @@
 <?php
     $route = new Router;
 
-    $route->get('/', ['PagesController', 'home']);
-    $route->get('about', ['PagesController', 'about']);
-    $route->get('search', ['PagesController', 'search']);
+    $route->get('/'      , ['PagesController', 'home']);
+    $route->get('about'  , ['PagesController', 'about']);
+    $route->get('search' , ['PagesController', 'search']);
 
-    $route->get('posts', ['PostsController', 'index']);
-    $route->get('posts/:id', ['PostsController', 'show']);
+    // $route->get('auth/login'     , ['AuthController', 'login']);
+    // $route->post('auth/login'    , ['AuthController', 'onLogin']);
+    // $route->get('auth/register'  , ['AuthController', 'register']);
+    // $route->post('auth/register' , ['AuthController', 'onRegister']);
+    // $route->get('auth/logout'    , ['AuthController', 'logout']);
+
+    $route->get('posts'     , ['PostsController', 'index']);
+    $route->get('posts/:id' , ['PostsController', 'show']);
 
     // if (isset($_SESSION['username'])) {
-        // $route->get('posts/create', ['PostsController', 'create']);
-        // $route->post('posts/store', ['PostsController', 'store']);
-        // $route->get('posts/:id/edit', ['PostsController', 'edit']);
-        // $route->post('posts/:id/update', ['PostsController', 'update']);
-        // $route->post('posts/:id/delete', ['PostsController', 'delete']);
+        // $route->get('posts/create'      , ['PostsController', 'create']);
+        // $route->post('posts/store'      , ['PostsController', 'store']);
+        // $route->get('posts/:id/edit'    , ['PostsController', 'edit']);
+        // $route->post('posts/:id/update' , ['PostsController', 'update']);
+        // $route->post('posts/:id/delete' , ['PostsController', 'delete']);
     // }
 
 
+
     $route->any(function() {
-        loadView('404');
+        // loadView('404');
+        return redirectTo();
     });
